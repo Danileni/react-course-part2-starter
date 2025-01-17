baf481eed1e962b58f9bdd7383b9c3c5cd7b34c2
Danileni <polykratish@gmail.com>
2025-01-18 00:47:30 +0200

    feat: add reducer for creating and deleting tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import './App.css';
-// import TodoForm from './react-query/TodoForm';
-// import TodoList from './react-query/TodoList';
-import Counter from "./state-management/Counter";
-// import PostList from './react-query/PostList';
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  // return <PostList/>
-  return (
-      <Counter />
-    // <>
-    //   <TodoForm />
-    //   <TodoList />
-    // </>
-  );
+  return <TaskList />
 }
 
 export default App;
