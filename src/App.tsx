8a2938b6f46924dd76eb10d5cc13c2ec8881209a
Danileni <polykratish@gmail.com>
2025-01-18 00:22:04 +0200

    feat: add reducer for incrementing and resetting counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
-import TodoForm from './react-query/TodoForm';
-import TodoList from './react-query/TodoList';
+// import TodoForm from './react-query/TodoForm';
+// import TodoList from './react-query/TodoList';
+import Counter from "./state-management/Counter";
 // import PostList from './react-query/PostList';
 
 function App() {
   // return <PostList/>
   return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
+      <Counter />
+    // <>
+    //   <TodoForm />
+    //   <TodoList />
+    // </>
   );
 }
 
