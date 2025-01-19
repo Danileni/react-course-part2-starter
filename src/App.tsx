1f386a434dd2b99f0286a1605fe03d85601fc81a
Danileni <polykratish@gmail.com>
2025-01-19 22:21:33 +0200

    feat: sharing state using react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useReducer } from "react";
 import './App.css';
-import TaskList from "./state-management/TaskList";
-import LoginStatus from "./state-management/LoginStatus";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./state-management/HomePage";
+import TasksContext from "./state-management/contexts/tasksContext";
+import tasksReducer from "./state-management/reducers/tasksReducer";
 
 function App() {
-  return <LoginStatus />
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
+  return (
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+  )
 }
 
 export default App;
